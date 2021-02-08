114306c858cd30b2ad45a4cd54c4403c2824bf40
Dmitry Rychkov <dmitry.rychkov@gmail.com>
2021-02-08 21:42:20 +0300

    doubly linked list

--- src/data-structures/doubly-linked-list/DoublyLinkedList.ts
+++ src/data-structures/doubly-linked-list/DoublyLinkedList.ts
@@ -13,6 +13,19 @@ export default class DoublyLinkedList<T> {
     this.compare = new Comparator<T>(compareFn);
   }
 
+  prepend(value: T): DoublyLinkedList<T> {
+    const node = new DoublyLinkedListNode(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      return this;
+    }
+    node.next = this.head;
+    this.head.previous = node;
+    this.head = node;
+    return this;
+  }
+
   append(value: T): DoublyLinkedList<T> {
     const node = new DoublyLinkedListNode(value);
     if (!this.head) {
@@ -31,6 +44,71 @@ export default class DoublyLinkedList<T> {
         this.tail = node;
       }
     }
+    return this;
+  }
+
+  delete(value: T): DoublyLinkedListNode<T> | null {
+    let deleted: DoublyLinkedListNode<T> | null = null;
+    let currentNode = this.head;
+    while (currentNode) {
+      if (this.compare.equals(currentNode.value, value)) {
+        deleted = currentNode;
+        if (currentNode.previous) {
+          currentNode.previous.next = currentNode.next;
+        } else {
+          this.head = currentNode.next;
+        }
+        if (currentNode.next) {
+          currentNode.next.previous = currentNode.previous;
+        } else {
+          this.tail = currentNode.previous;
+        }
+      }
+      currentNode = currentNode.next;
+    }
+    return deleted;
+  }
+
+  deleteTail(): DoublyLinkedListNode<T> | null {
+    if (!this.tail) {
+      return null;
+    }
+    if (this.tail === this.head) {
+      const deletedNode = this.tail;
+      this.tail = null;
+      this.head = null;
+      return deletedNode;
+    }
+    const deletedNode: DoublyLinkedListNode<T> = this.tail;
+    this.tail = this.tail.previous;
+    if (this.tail) {
+      this.tail.next = null;
+    }
+    return deletedNode;
+  }
+
+  deleteHead(): DoublyLinkedListNode<T> | null {
+    if (!this.head) {
+      return null;
+    }
+    if (this.head === this.tail) {
+      const deletedNode = this.head;
+      this.head = null;
+      this.tail = null;
+      return deletedNode;
+    }
+    const deletedNode = this.head;
+    this.head = this.head.next;
+    if (this.head) {
+      this.head.previous = null;
+    }
+    return deletedNode;
+  }
+
+  fromArray(array: T[]): DoublyLinkedList<T> {
+    array.forEach((value) => {
+      this.append(value);
+    });
     return this;
   }
 
